441db9f5e960d5197a4eda03e64ec4d7c4c39bd7
Steven Brooks Mogbo <stevenmogbo@yahoo.com>
2020-10-10 13:04:44 -0400

    added js file

--- models/custom_services.js
+++ models/custom_services.js
@@ -1,13 +1,15 @@
-module.exports = function (sequelize, DataTypes) {
-  const Custom_serives = sequelize.define('Custom_serives', {
+module.exports = function(sequelize, DataTypes) {
+  // eslint-disable-next-line camelcase
+  const Custom_serives = sequelize.define("Custom_serives", {
     name: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: false
     },
     price: {
-      type: DataTypes.DECIMAL(10,2),
-      allowNull: false,
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false
     }
   });
+  // eslint-disable-next-line camelcase
   return Custom_serives;
 };
