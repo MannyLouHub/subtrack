c91202b88ed2e2dbf6307e58f8a86f69969850a0
Steven Brooks Mogbo <stevenmogbo@yahoo.com>
2020-10-11 16:10:48 -0400

    added code code for sutom js

---
+++ public/js/custom_services.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable camelcase */
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(() => {
+  $(".create-form").on("submit", event => {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    const customSub = {
+      custom_name: $("#customSub")
+        .val()
+        .trim()
+    };
+
+    // Send the POST request.
+    $.ajax("/api/custom_services", {
+      type: "POST",
+      data: customSub
+    }).then(() => {
+      console.log("created new Subscription");
+      // Reload the page to get the updated list
+      location.reload();
+    });
+  });
+});
