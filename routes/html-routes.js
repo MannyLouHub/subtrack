251e59248a3ec6a2e83f1305d936db43c2c83093
Mark Clarke <the9k9@yahoo.com>
2020-10-15 18:41:10 -0400

    sign up page as well.

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -18,7 +18,7 @@ module.exports = function(app) {
         if (req.user) {
             res.redirect("/members");
         }
-        res.sendFile(path.join(__dirname, "../public/login.html"));
+        //res.sendFile(path.join(__dirname, "../public/login.html"));
 
         res.sendFile(path.join(__dirname, "../public/signup.html"));
 
